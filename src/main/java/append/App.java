4df2535afc6cb2d243ff27f5939c4f0071acce6a
n0rb1v <n0rb1v@gmail.com>
2021-04-27 15:31:15 +0200

    2

--- src/main/java/append/App.java
+++ src/main/java/append/App.java
@@ -1,20 +1,28 @@
 package append;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class App {
 
-    public int[] append(int[] a1, int[]a2){
-        int[] result = new int[a1.length + a2.length];
-        int index= 0;
-
-        for(int i = 0; i< a1.length; i++){
-            result[index] =a1[i];
-            index++;
-        }
-        for(int i = 0; i< a2.length; i++){
-            result[index] =a2[i];
-            index++;
-        }
-        return result;
-
+    public int[] append(int[] a1, int[] a2) {
+//        int[] result = new int[a1.length + a2.length];
+//        int index = 0;
+//
+//        for (int i = 0; i < a1.length; i++) {
+//            result[index] = a1[i];
+//            index++;
+//        }
+//        for (int i = 0; i < a2.length; i++) {
+//            result[index] = a2[i];
+//            index++;
+//        }
+//        return result;
+        List<Integer> tmp = new ArrayList<>();
+        tmp.addAll(Arrays.stream(a1).boxed().collect(Collectors.toList()));
+        tmp.addAll(Arrays.stream(a2).boxed().collect(Collectors.toList()));
+        return tmp.stream().mapToInt(Integer::intValue).toArray();
     }
 }
